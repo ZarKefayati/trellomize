03fcbc894da0a661e83c9f9a308a16a9c548ee23
ZarKefayati <zar.k.com@gmail.com>
2024-05-25 02:04:50 +0330

    design & debug

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import json
-
-
+from cryptography.fernet import Fernet
+from rich import print
+from rich.panel import Panel
+from rich.text import Text
+from rich.console import Console
 
 class User:
     def __init__(self, Email, username, password, active):
@@ -10,45 +13,62 @@ class User:
         self.password = password
         self.active = active
     def setUsername(self, username , file):
-        if username in file:
-            username = input("The username is already available. \nEnter another one \n(1. exit)")
-            if username == '1':
+        if username in file: #Duplicate username
+            username = input ("Username already exists. Enter username or 1 to exit:")
+            if username == '1': 
                 menu()
                 return
+            else:
+                self.setUsername(username, file)
+                return
 
-        elif  len(username) < 4 or len(username) > 10:
-            self.setUsername(input("Characters must be between 4 and 10. Enter another one:"), file)
-        else:
+        elif  len(username) < 4 or len(username) > 10: #incorrect username
+            self.setUsername(input("Characters must be between 4 and 10. \nEnter another one:"), file)
+        else: #correct username & save
             self.username = username
 
+class field:
+    pass
 
+class project:
+    def __init__(self, Leader, ID, Title, Fields, Users): 
+        self.Leader = Leader
+        self.ID = ID
+        self.Title = Title
+        self.Fields = Fields
+        self.Users = Users
 
-def sign_in ():
-    username = input('Enter username: ')
-    password = input('Enter password: ')
-    if os.path.exists("Users.json"):
-        with open("Users.json") as file:
-            data = json.load(file)
-        if username in data:
-            #decryption
-            from cryptography.fernet import Fernet
-            with open("mykey.key", 'rb') as mykey:
-                key = mykey.read()
-            f = Fernet(key)
-            with open(username + '.json', 'rb') as encrypted_file:
-                encrypted = encrypted_file.read()
-            decrypted = f.decrypt(encrypted)
-            information = eval(decrypted.decode())
-            if information['password'] == password: #check password
-                print ('yes')
-
+def sign_in (username, password):
+    #decryption
+    if os.path.exists('users/' + username + ".json"):
+        with open("mykey.key", 'rb') as mykey: #receive key 
+            key = mykey.read()
+        f = Fernet(key)
+        with open('users/' + username + '.json', 'rb') as encrypted_file: #receive user information 
+            encrypted = encrypted_file.read()
+        decrypted = f.decrypt(encrypted) #decryption
+        information = eval(decrypted.decode()) #byte to str
+        if information['password'] == password: #check password
+            if information["active"] == 'Active':
+                print(Text('yes', 'bold green')) 
+            else:
+                print('no')
         else:
-            print('Username is not exsits. create an account first.')
-            create_acount()
+            k = input('Password is incorrect.\nEnter your password or 1 to exit:')      
+            if k == '1': 
+                menu()
+                return
+            else:
+                sign_in(username, k)
+                return
     else:
-        print('Username is not exsits. create an account first.')
-        create_acount()
-
+        k = input("Username is not exsits. \nEnter username or 1 to exit:")
+        if k == '1': 
+            menu()
+            return
+        else:
+            sign_in(k, password)
+            return
 
 def create_acount ():
     User1 = User("", "", "", "Active")
@@ -76,13 +96,12 @@ def create_acount ():
 
 
     #adding information in another file
-    file = open(User1.username + ".json" , 'w')
+    file = open('users/' + User1.username + ".json" , 'w')
     item = {"Email" : User1.Email , "username" : User1.username , "password" : User1.password , "active" : User1.active}
     json.dump(item, file, indent=4)
     file.close()
 
     #encryption
-    from cryptography.fernet import Fernet
     if not os.path.exists('mykey.key'): #create key & save
         key = Fernet.generate_key()
         with open('mykey.key', 'wb') as mykey:
@@ -92,18 +111,31 @@ def create_acount ():
             key = mykey.read()
 
     f = Fernet(key)
-    with open(User1.username + ".json", 'rb') as original_file:
+    with open('users/' + User1.username + ".json", 'rb') as original_file:
         original = original_file.read()
     encrypted = f.encrypt(original) 
-    with open(User1.username + ".json", 'wb') as encrypted_file:
+    with open('users/' + User1.username + ".json", 'wb') as encrypted_file:
         encrypted_file.write(encrypted)
 
-
 def menu():
-    k = input('1. create account\n2. sign in\n')
+    from rich import print
+    from rich.panel import Panel
+    from rich.text import Text
+    from rich.console import Console
+    console = Console()
+    panel = Panel(Text('1. create account\n2. sign in', justify="left")) 
+    k = console.input(panel)
     if k == '1':
         create_acount()
     elif k == '2':
-        sign_in()
+        from rich.console import Console
+        from rich.text import Text
+        console = Console()
+        username = console.input(Text("Enter username: ","bold green"))
+        password = console.input(Text("Enter password: ","bold green"))
+        sign_in(username, password)
+
+def Account_page(username):
+    pass
 
 menu()
